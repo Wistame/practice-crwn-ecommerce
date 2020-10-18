85c930fcb6a54db7a01ee5ea67411e6c1c9a94af
roatencio <roatencio@globaltask.net>
2020-10-18 00:54:49 -0300

    collection-item structure and predefined css added

--- src/components/preview-collection/CollectionPreview.jsx
+++ src/components/preview-collection/CollectionPreview.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-
-
-const CollectionPreview = ({title, items,}) =>(
+import CollectionItem from '../collectionItem/CollectionItem'
+import './CollectionPreview.scss'
+const CollectionPreview = ({title, items}) =>(
     <div className='collection-preview'> 
         <h1 className>{title.toUpperCase()}</h1>
-        <div ClassName='preview'>
+        <div className='preview'>
             {items
-                .filter((item, idx) => idx > 4)
-                .map(item =>(
-                    <div key={item.id}> {item.name}</div>
+                .filter((item, idx) => idx < 4)
+                .map(({id, ...CollectionPreviewProps }) =>(
+                    <CollectionItem key={id} {...CollectionPreviewProps}/>
                 ))
             }
 
